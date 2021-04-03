5937661095568bfe03c3d3cc1b76e836dd5d4632
Kat <katrinaking25@gmail.com>
2021-04-02 22:16:51 -0600

    Initial commit

---
+++ MyGram/wwwroot/js/Index.js
@@ -0,0 +1,13 @@
+﻿$("#MainContainer").append("<span>My Gram </span>");
+$("#MainContainer").append("<button>Log In </button>");
+$("#MainContainer").append("&nbsp;");
+$("#MainContainer").append("<button> Sign up</button>");
+
+$("#MainContainer").append("<br>");
+$("#MainContainer").append("<br>");
+$("#MainContainer").append("<span>Image 1 Image 2 Image 3 Image 4 Image 5</span>");
+$("#MainContainer").append("<br>");
+$("#MainContainer").append("<br>");
+$("#MainContainer").append("<span>About Blog Jobs Help </span>");
+$("#MainContainer").append("<br>");
+$("#MainContainer").append("<span>English 2021 MyGram </span>");
