b04ae90a51635f90330e2e6290da4398a370337d
Kat <katrinaking25@gmail.com>
2021-04-05 18:46:41 -0600

    login and signup elements added

--- MyGram/wwwroot/js/Index.js
+++ MyGram/wwwroot/js/Index.js
@@ -1,9 +1,9 @@
-﻿// testing for github
-$("#MainContainer").append("<span>My Gram </span>");
+﻿
+/*$("#MainContainer").append("<span>My Gram </span>");
 $("#MainContainer").append("<button>Log In </button>");
 $("#MainContainer").append("&nbsp;");
 $("#MainContainer").append("<button> Sign up</button>");
-
+*/
 $("#MainContainer").append("<br>");
 $("#MainContainer").append("<br>");
 $("#MainContainer").append("<span>Image 1 Image 2 Image 3 Image 4 Image 5</span>");
@@ -12,3 +12,17 @@ $("#MainContainer").append("<br>");
 $("#MainContainer").append("<span>About Blog Jobs Help </span>");
 $("#MainContainer").append("<br>");
 $("#MainContainer").append("<span>English 2021 MyGram </span>");
+
+
+$("#login").click(function () {
+    alert("You have logged in");
+});
+
+$("#signup").click(function () {
+    
+    $("#logindiv").append("<br>");
+    $("#logindiv").append("<span>User</span>");
+    $("#logindiv").append("<br>");
+    $("#logindiv").append("<span>Password</span>");
+    $("#logindiv").append("<br>");
+});
